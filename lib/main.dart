e64caee1406db7e4911d9a7bf098b88fe00fc290
pptavozl <pptavozl@gmail.com>
2019-08-16 13:39:02 -0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ZimbronApps Ejemplo',
+      debugShowCheckedModeBanner: false,
+      home: MiHome(),
+    );
+  }
+}
+
+class MiHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Row'),
+      ),
+      body: Center(
+        child: Container(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.thumb_up),
+                color: Colors.green,
+                onPressed: (){},
+              ),
+              RaisedButton(
+                child: Text("Botón"),
+                onPressed: () {},
+              ),
+              FittedBox(
+                fit: BoxFit.contain, 
+                child: const FlutterLogo(),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
